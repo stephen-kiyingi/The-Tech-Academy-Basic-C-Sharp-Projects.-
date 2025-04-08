1ed659a504697ea76cc144a4f2259f2a66deadab
stephen-kiyingi <steverone1989@gmail.com>
2025-04-08 11:09:40 -0700

    Add files via upload

---
+++ ConsoleAppAssignment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //do while loop
+            int count = 0;
+            do
+            {
+                Console.WriteLine("inside the loop");
+                count++;// add one each time
+            }
+            while (count < 5);//continue to loop untill 5
+
+            //while loop
+            int counting = 0;
+            while (counting < 3)//continue to loop til reaches 3
+            {
+                Console.WriteLine("counting  numbers");
+                count++; //add one each time
+            }
+            Console.ReadLine();
+        }
+    }
+}
