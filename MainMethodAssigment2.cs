abbd7cf6ff58a40c395a8ee8a5b311f4db3d9f3f
stephen-kiyingi <steverone1989@gmail.com>
2025-05-11 13:13:19 -0700

    Add files via upload

---
+++ MainMethodAssigment2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//program.cs
+namespace MainMethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instatiate the MathMethods2 class
+            MathMethods2 math1 = new MathMethods2();
+            // call the first method within an in parameter
+            Console.WriteLine(math1.MathOp(7));
+            //call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(7.3m));
+            //call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("7"));
+        }
+    }
+
+}
---
+++ Method Assignment 4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class MathOperation1
+    {
+        //create a method that takes two intergers as parameters
+        //make one parameter optional by providing a default value
+        public int MathOperation(int i, int j = 1)
+        {
+            //perform multiplication on intergers and return the result 
+            int result = i * j;
+            return result;
+        }
+
+    }
+}
